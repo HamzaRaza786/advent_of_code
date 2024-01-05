781134b60bf1cc6544c91b8f7230158676a2daa7
HamzaRaza786 <hamza.raza2015@outlook.com>
2024-01-06 04:45:38 +0500

    Test case passing

--- day-07/src/bin/solution.rs
+++ day-07/src/bin/solution.rs
@@ -1,4 +1,6 @@
 use std::{
+    char,
+    collections::{HashMap, HashSet},
     i64::{self, MAX},
     ops::Range,
 };
@@ -22,12 +24,13 @@ impl GridRepresentation {
         Self::default()
     }
 }
-#[derive(Default, Debug)]
+#[derive(Default, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct SingleHand {
     card: Vec<char>,
     bid: i64,
-    score: i64,
+    score: usize,
 }
+
 pub fn read_lines(_pathname: &str) -> Vec<String> {
     include_str!("./input.txt")
         .split('\n')
@@ -38,6 +41,23 @@ pub fn read_lines(_pathname: &str) -> Vec<String> {
 impl Runner for GridRepresentation {
     fn parse(&mut self) {
         let lines = read_lines("./input_test.txt");
+        let dict_mapping: HashMap<char, usize> = HashMap::from([
+            ('A', 14),
+            ('K', 13),
+            ('Q', 12),
+            ('J', 11),
+            ('T', 10),
+            ('9', 9),
+            ('8', 8),
+            ('7', 7),
+            ('6', 6),
+            ('5', 5),
+            ('4', 4),
+            ('3', 3),
+            ('2', 2),
+            ('1', 1),
+        ]);
+
         for line in lines {
             let line_split = line.split_whitespace().collect::<Vec<_>>();
             let cards = line_split[0].chars().collect::<Vec<_>>();
@@ -49,13 +69,39 @@ impl Runner for GridRepresentation {
                 score: 0,
             })
         }
-        for card in &self.hands {
-            for letter in card.card {
+        for card in &mut self.hands {
+            let uniq_card = card
+                .card
+                .iter()
+                .map(|c| c)
+                .collect::<HashSet<&char>>()
+                .into_iter()
+                .collect::<Vec<&char>>();
+            let mut score = 0;
+            for letter in uniq_card {
+                let count = card.card.iter().filter(|&ch| ch == letter).count();
+                if count > 1 && count > score {
+                    score = count;
+                }
             }
-            println!("{:?}", card.card);
+            let mut temp_score = 0;
+            let base: usize = 10usize; // an explicit type is required
+            for (i, val) in card.card.iter().enumerate() {
+                temp_score += dict_mapping.get(val).unwrap() * base.pow(5 - i as u32);
+            }
+            card.score = temp_score << score;
         }
+        self.hands.sort_by(|a,b| a.score.cmp(&b.score));
     }
-    fn part1(&mut self) {}
+    fn part1(&mut self) {
+       let mut rank = 0;
+        for (i,hand) in self.hands.iter().enumerate() {
+            
+            rank+= hand.bid * (i as i64 + 1);
+        }
+        println!("Rank {}", rank);
+    }
+
     fn part2(&mut self) {}
     //
 }
